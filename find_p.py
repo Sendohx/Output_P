596318337d9ad086b54484305585133cb231f7d1
Sendohx <151484757+Sendohx@users.noreply.github.com>
2023-12-07 09:44:17 +0800

    find_p.py

--- find_p.py
+++ find_p.py
@@ -1,5 +1,5 @@
 # -*- coding = utf-8 -*-
-# @Time: 2023/11/28 12:52
+# @Time: 2023/12/7 9:44
 # @Author: Jiahao Xu
 # @File：find_p.py
 # @Software: PyCharm
@@ -7,20 +7,21 @@
 import pandas as pd
 
 
-def find_p(data, factor):
+def find_p(data, factor_list):
     """
     计算indicators的p分位值（0-90）
-    :param data: 数据的dataframe
+    :param data:
     :param factor: 因子名称
     :return: p分位值序列
     """
-    factor_ranks = data[factor].rolling(242).apply(
-            lambda x: pd.Series(x).rank(pct=True, na_option='keep').iloc[-1] * 100)
-    if pd.isna(factor_ranks):
-        return None
-    else:
-        values = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
-        data[factor + '_P'] = max(val for val in values if val < factor_ranks)
+    for factor in factor_list:
+        factor_ranks = data[factor].rolling(242).apply(
+                lambda x: pd.Series(x).rank(pct=True, na_option='keep').iloc[-1] * 100)
+        if pd.isna(factor_ranks):
+            return None
+        else:
+            values = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+            data[factor + '_P'] = max(val for val in values if val < factor_ranks)
 
     return data
 
@@ -29,6 +30,6 @@ if __name__=='__main__':
 
     data = {}
     data = pd.DataFrame(data)
-    factor = 'xxx'
+    factor_list = ['a', 'b', 'c']
 
-    data = find_p(data, factor)
+    data = find_p(data, factor_list)
